8375ddbfdc7727be6738f2c37861dae8f977383e
GayathriThangaraj <125265735+GayathriThangaraj@users.noreply.github.com>
2023-12-09 14:32:49 +0530

    This repository consists of react js

---
+++ REACT NODE JS/CLASSWORK/helloworld/src/App.js
@@ -0,0 +1,11 @@
+import Sample from './Sample.js'
+import './App.js'
+function App(){
+  return (
+    <div>
+      <Sample/>
+       <h1>Hello World!</h1>
+    </div>
+  )
+}
+export default App
---
+++ REACT NODE JS/CLASSWORK/helloworld/src/Sample.js
@@ -0,0 +1,18 @@
+function Sample()
+{
+    return(
+        <div>
+            <p>This is a para componenet</p>
+        </div>
+    );
+}
+export default Sample
+/*function sum(...args){
+    let sum=0;
+    for(let i=0;i<args.length;i++){
+        sum+=args[i];
+    }
+    return sum;
+}
+console.log(sum(2,4,5,6))
+console.log(sum(20,30,40))*/
